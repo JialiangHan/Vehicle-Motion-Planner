4b6f7425ab6ad884b59886285b6f3e39497e4496
Jialiang Han <hanjiali@umich.edu>
2021-04-09 16:47:59 +0800

    add some files for Hybrid_A_star.py

---
+++ Hybrid A/Node_2D.py
@@ -0,0 +1,7 @@
+"""
+this class is for 2D a star to find out unconstrained heuristic
+"""
+
+
+class Node_2D():
+    def __init__(self,x,y):
---
+++ Hybrid A/main.py
@@ -0,0 +1,15 @@
+
+
+generate a grid map
+
+offline calculate constrained heuristic
+
+generate a grid map with obstacle
+
+use hybrid_A_star()
+
+path smoothing
+
+plot
+
+

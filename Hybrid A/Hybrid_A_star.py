1e0ea79ec9d24fe9665dda45e068b76dffc21a8e
Jialiang Han <hanjiali@umich.edu>
2021-04-18 23:55:48 +0800

    starting working on Hybrid_A_star.py

--- Hybrid A/Hybrid_A_star.py
+++ Hybrid A/Hybrid_A_star.py
@@ -1,7 +1,73 @@
-import math
-import time
-from matplotlib import pyplot as plt
-import numpy as np
+"""
+this A star is only for calculating unconstrained heuristic for hybrid A
+"""
+import Map
+from Node_3D import *
+import matplotlib.pyplot as plt
+import Vehicle_model
 
 
+class Hybrid_A_star:
+    def __init__(self, start: Node_3D, goal: Node_3D, map: Map):
+        self.start = start
+        self.goal = goal
+        self.map = map
+        self.open_list = dict()
+        self.close_list = dict()
 
+    def run(self):
+        width = self.map.size[1]
+        self.start.update_heuristic(self.goal)
+        self.start.update_cost_so_far()
+        self.start.get_f_value()
+        self.start.set_index(width)
+        self.open_list[self.start.index] = self.start
+        while len(self.open_list.keys()) > 0:
+            current_index = min(self.open_list, key=lambda x: self.open_list[x].f_value)
+            current_node = self.open_list[current_index]
+            del self.open_list[current_index]
+            self.close_list[current_index] = current_node
+            if current_node == self.goal:
+                self.goal = current_node
+                break
+            successor = current_node.create_successor()
+            for succ in successor:
+                if succ.is_on_grid(self.map):
+                    succ.set_index(self.map.size[1])
+                    if succ.index not in self.close_list:
+                        if self.grid_map.grid_map[succ.index] != 1:
+                            succ.update_cost_so_far()
+                        else:
+                            succ.cost_so_far = 10000
+                        succ.update_heuristic(self.goal)
+                        succ.get_f_value()
+                        if succ.index not in self.open_list:
+                            self.open_list[succ.index] = succ
+                        else:
+                            if succ.cost_so_far < self.open_list[succ.index].cost_so_far:
+                                self.open_list[succ.index] = succ
+
+    def get_path(self):
+        path_x, path_y,path_theta = [self.goal.x], [self.goal.y],[self.goal.theta]
+        current_node = self.goal
+        while 1:
+            current_node = current_node.Predecessor
+            path_x.append(current_node.x)
+            path_y.append(current_node.y)
+            path_theta.append(current_node.theta)
+            if current_node == self.start:
+                break
+        return path_x, path_y,path_theta
+
+    def plot_path(self):
+        self.map.Plot()
+        path_x,path_y,path_theta=self.get_path()
+        plt.plot(path_x, path_y, "-r", label="Hybrid A* path")
+
+    def plot_vehicle(self):
+        path_x, path_y, path_theta = self.get_path()
+        for x,y,yaw in zip(path_x,path_y,path_theta):
+            plt.grid(True)
+            plt.axis("equal")
+            Vehicle_model.plot_car(x, y, yaw)
+            plt.pause(0.0001)
---
+++ Hybrid A/Node_3D.py
@@ -0,0 +1,70 @@
+"""
+this class is for 3D(x,y,theta)
+"""
+import distance
+
+
+class Node_3D:
+    def __init__(self, x: int, y: int,theta:float, cost_so_far=None, heuristic=None, predecessor=None):
+        self.x = x
+        self.y = y
+        self.theta= theta
+        if cost_so_far is not None:
+            self.cost_so_far = cost_so_far
+        else:
+            self.cost_so_far = 0
+        self.heuristic = heuristic
+        if predecessor is None:
+            self.Predecessor = self
+        else:
+            self.Predecessor = predecessor
+        self.in_open_list = False
+        self.in_close_list = False
+        self.discover = False
+        self.index = -1
+        self.f_value=0
+
+    def set_index(self, map_width):
+        self.index = self.x + self.y * map_width
+
+    def get_f_value(self):
+        self.f_value=self.cost_so_far + self.heuristic
+
+    def cost(self, other):
+        return distance.dist(self, other)
+
+    def update_heuristic(self, goal):
+        self.heuristic = self.cost(goal)
+
+    def is_on_grid(self, map) -> bool:
+        if map.size[0] <= self.x < map.size[1] and map.size[2] <= self.y < map.size[3]:
+            return True
+        else:
+            return False
+
+    def update_cost_so_far(self):
+        self.cost_so_far = self.cost_so_far + self.cost(self.Predecessor)
+        self.discover = True
+
+    def create_successor(self):
+        direction = 8
+        dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+        dy = [0, 1, 1, 1, 0, -1, -1, -1]
+        successor = []
+        for i in range(direction):
+            x_successor = self.x + dx[i]
+            y_successor = self.y + dy[i]
+            successor.append(Node_2D(x_successor, y_successor, self.cost_so_far, 0, self))
+        return successor
+
+    def __gt__(self, other):
+        return self.f_value>other.f_value
+
+    def __eq__(self, other):
+        if self.x==other.x and self.y==other.y and self.theta==other.theta:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "x: " + str(self.x)[:4] + ", y: " + str(self.y)[:4]+ ", theta: " + str(self.theta)[:4]

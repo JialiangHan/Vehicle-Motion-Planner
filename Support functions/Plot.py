fbd163b5c225fe4327ed87e862bc25a0b9a1e7e9
Jialiang Han <hanjiali@umich.edu>
2021-04-18 17:04:10 +0800

    update Plot.py: update plot.polygon: add color arguement.

--- Support functions/Plot.py
+++ Support functions/Plot.py
@@ -7,18 +7,24 @@ def plot_Node(node):
     plt.plot(node.x, node.y, ".k")
 
 
-def plot_Edge(edge,figure):
-    figure.plot([edge.start.x, edge.end.x], [edge.start.y, edge.end.y], "k")
+def plot_Edge(edge, figure=None):
+    if figure == None:
+        plt.plot([edge.start.x, edge.end.x], [edge.start.y, edge.end.y], "k")
+    else:
+        figure.plot([edge.start.x, edge.end.x], [edge.start.y, edge.end.y], "k")
     # plt.show()
 
 
-def plot_Polygon(polygon, fill_or_not: bool,figure):
+def plot_Polygon(polygon, fill_or_not: bool, figure=None,color=None):
     x, y = [], []
     for edge in polygon.edge_list:
-        plot_Edge(edge,figure)
+        plot_Edge(edge, figure)
     for vertex in polygon.vertices:
         x.append(vertex.node.x)
         y.append(vertex.node.y)
     if fill_or_not is True:
-        plt.fill(x, y, "b")
+        if color==None:
+            plt.fill(x, y, "b")
+        else:
+            plt.fill(x,y,color)
     # plt.show()

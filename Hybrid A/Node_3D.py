f9892f6c79b726ad345cb2431c488b2d83631ced
Jialiang Han <hanjiali@umich.edu>
2021-04-20 00:03:10 +0800

    finished create successor function. next is to finish update cost and update heuristic

--- Hybrid A/Node_3D.py
+++ Hybrid A/Node_3D.py
@@ -1,14 +1,16 @@
 """
 this class is for 3D(x,y,theta)
 """
+import Angle
 import distance
+import math
 
 
 class Node_3D:
-    def __init__(self, x: int, y: int,theta:float, cost_so_far=None, heuristic=None, predecessor=None):
+    def __init__(self, x: float, y: float, theta: float, cost_so_far=None, heuristic=None, predecessor=None):
         self.x = x
         self.y = y
-        self.theta= theta
+        self.theta = theta
         if cost_so_far is not None:
             self.cost_so_far = cost_so_far
         else:
@@ -22,10 +24,10 @@ class Node_3D:
         self.in_close_list = False
         self.discover = False
         self.index = -1
-        self.f_value=0
+        self.f_value = 0
 
     def get_f_value(self):
-        self.f_value=self.cost_so_far + self.heuristic
+        self.f_value = self.cost_so_far + self.heuristic
 
     def cost(self, other):
         return distance.dist(self, other)
@@ -44,13 +46,35 @@ class Node_3D:
         self.discover = True
 
     def __gt__(self, other):
-        return self.f_value>other.f_value
+        return self.f_value > other.f_value
 
     def __eq__(self, other):
-        if self.x==other.x and self.y==other.y and self.theta==other.theta:
+        if self.x == other.x and self.y == other.y and self.theta == other.theta:
             return True
         else:
             return False
 
+    def create_successor(self):
+        successors = []
+        R = 6  # [m] turning radius
+        steering_angle = 6.75  # [deg]
+        d_theta = math.radians(steering_angle)
+        dx = R * math.sin(d_theta)
+        dy = R * (1 - math.cos(d_theta))
+        delta_theta = [0, d_theta, -d_theta]
+        delta_x = [R * d_theta, dx, dx]
+        delta_y = [0, dy, -dy]
+        for i in range(6):
+            if i < 3:  # forward
+                x = self.x + delta_x[i] * math.cos(self.theta) - delta_y * math.sin(self.theta)
+                y = self.y + delta_x[i] * math.sin(self.theta) + delta_y * math.cos(self.theta)
+                theta = Angle.convert_into_2pi(self.theta + delta_theta[i])
+            else:  # backward
+                x = self.x - delta_x[i - 3] * math.cos(self.theta) - delta_y[i - 3] * math.sin(self.theta)
+                y = self.y - delta_x[i - 3] * math.sin(self.theta) + delta_y[i - 3] * math.cos(self.theta)
+                theta = Angle.convert_into_2pi(self.theta - delta_theta[i - 3])
+            successors.append(Node_3D(x, y, theta, self.cost_so_far, None, self))
+        return successors
+
     def __str__(self):
-        return "x: " + str(self.x)[:4] + ", y: " + str(self.y)[:4]+ ", theta: " + str(self.theta)[:4]
+        return "x: " + str(self.x)[:4] + ", y: " + str(self.y)[:4] + ", theta: " + str(self.theta)[:4]

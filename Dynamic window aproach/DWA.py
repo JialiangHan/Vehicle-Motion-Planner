321615a3bdb5da6d7518c97770a52a309c726b63
Jialiang Han <hanjiali@umich.edu>
2021-05-13 00:24:43 +0800

    working on DWA

--- Dynamic window aproach/DWA.py
+++ Dynamic window aproach/DWA.py
@@ -10,21 +10,46 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))
                 + "/../Support functions")
 
 import Vehicle_model
-
+import Node_3D
 
 class dynamic_window_approach:
-    def __init__(self,start,goal,map,config):
+    def __init__(self,start:Node_3D,goal:Node_3D,map:Map,config:Config):
         self.start=start
         self.goal=goal
         self.map=map
         self.config=config
 
+    def run(self):
+        #state is initial state, should equal to self.start
+        state=[self.start.]
+        dw=self.get_dynamic_window(state)
+        u,trajectory = self.get_trajectory(state,dw)
+        
 
+    
+
+    
+    def get_trajectory(self):
+
+
+    def get_dynamic_window(self,state:list)->list:
+        # state = [x,y,heading angle, velocity, steering angle]
+        #use bicycle model, dynamic window should be [velocity, steering angle]
+        Vs = [self.config.min_speed,self.config.max_speed,
+         -self.config.max_steering_angle,self.config.max_steering_angle]
+        
+        Vd = [state[3]-self.config.max_accel * self.config.dt,
+        state[3]+self.config.max_accel*self.config.dt,
+        state[4]-self.config.max_steering_angle_rate*self.config.dt,
+        state[4]+self.config.max_steering_angle_rate*self.config.dt]
+
+        dw=[max(Vs[0],Vd[0]),min(Vs[1],Vd[1]),max(Vs[2],Vd[2]),min(Vs[3],Vd[3])]
+
+        return dw
+        
     def calculate_total_cost(self):
-    def
-    def get_dynamic_window(self):
 
-
+    def plot_path(self):
 
 
 
--- Dynamic window aproach/main.py
+++ Dynamic window aproach/main.py
@@ -23,24 +23,17 @@ class Config:
         # robot parameter
         self.max_speed = 1.0  # [m/s]
         self.min_speed = -0.5  # [m/s]
-        self.max_yaw_rate = 40.0 * math.pi / 180.0  # [rad/s]
+        self.max_steering_angle = 40.0 * math.pi / 180.0  # [rad/s]
         self.max_accel = 0.2  # [m/ss]
-        self.max_delta_yaw_rate = 40.0 * math.pi / 180.0  # [rad/ss]
+        self.max_steering_angle_rate = 40.0 * math.pi / 180.0  # [rad/ss]
         self.v_resolution = 0.01  # [m/s]
-        self.yaw_rate_resolution = 0.1 * math.pi / 180.0  # [rad/s]
+        self.steering_angle_resolution = 0.1 * math.pi / 180.0  # [rad/s]
         self.dt = 0.1  # [s] Time tick for motion prediction
         self.predict_time = 3.0  # [s]
         self.to_goal_cost_gain = 0.15
         self.speed_cost_gain = 1.0
         self.obstacle_cost_gain = 1.0
         self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
-        self.robot_type = RobotType.circle
-
-        # if robot_type == RobotType.circle
-        # Also used to check if goal is reached in both types
-        self.robot_radius = 1.0  # [m] for collision check
-
-        # if robot_type == RobotType.rectangle
         self.robot_width = 0.5  # [m] for collision check
         self.robot_length = 1.2  # [m] for collision check
 
